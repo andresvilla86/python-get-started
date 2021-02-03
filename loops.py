56284fa5ef5b9379f48b30ba950d70826794a998
andres <andres.villa.rivas@gmail.com>
2021-02-03 09:02:53 -0500

    Agregando nuevos ejemplos

--- loops.py
+++ loops.py
@@ -1,3 +1,15 @@
-fruits = ["apple", "banana", "cherry"]
+#Loop "for" examples
+
+car_brands = ["Ferrari", "Tesla", "Jaguar"]
+for x in car_brands:
+  print(x)
+
+for x in "Ferrari":
+ print(x)
+
+#The break statement
+car_brands = ["Ferrari", "Tesla", "Jaguar"]
 for x in fruits:
   print(x)
+  if x == "Tesla":
+    break
